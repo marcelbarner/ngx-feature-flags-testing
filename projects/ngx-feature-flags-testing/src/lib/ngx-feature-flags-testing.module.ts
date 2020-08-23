43c76ba61d8d51975e4213ce872770bb8ca30539
Marcel Barner <marcel.barner@web.de>
2020-08-23 18:15:28 +0200

    Revert "Develop" (#11)
    * Revert "chore(release): 1.2.0"
    
    This reverts commit 65dc485bada8d368e806ef7bbb6c41a7585fd76f.
    
    * Revert "feat: 🎸 Require testing before push"
    
    This reverts commit fa76bd396773c316060c3cb29745d05fa110ef7f.
    
    * Revert "chore(release): 1.1.1"
    
    This reverts commit e8dafc9cd57421c02548b24b5d641d7e4d01802f.
    
    * Revert "docs: ✏️ adjust readme"
    
    This reverts commit 5b78f442558bbba1dc2d228b9050c122c2689ee6.
    
    * Revert "fix: 🐛 Fix build"
    
    This reverts commit fcb6df7d67b529f271b27c483a0662ab375059ba.
    
    * Revert "chore(release): 1.1.0"
    
    This reverts commit 99cd7aeaaa090ffc177bf3ca71d8285ca2fa8fd1.
    
    * Revert "feat: 🎸 Adding state changes"
    
    This reverts commit a5be9a31f36e8de8233d7e38ba49e9cb19050990.
    
    * Revert "feat: 🎸 Adding multiple flags during import"
    
    This reverts commit a20d69d3c769ba97f6d5ee3a93c07d4dc80186aa.
    
    * Revert "docs: ✏️ Addaped ReadMe"
    
    This reverts commit c30206efaf77b628fb3acc7c4814d376a01fe162.
    
    * Revert "feat: 🎸 Possibility to add single flag during import"
    
    This reverts commit df114af4a22c2d2cf1d349a87cce2c43003acd54.
    
    * Revert "feat: 🎸 Offer a simple import"
    
    This reverts commit 4b47470d28a1a02c94c4cdc6d37eda43bd752391.
    
    * Revert "perf: ⚡️ Remove obsolete files"
    
    This reverts commit 0660e121d0f74e331b32d09a4b0de160bd6ed29a.

--- projects/ngx-feature-flags-testing/src/lib/ngx-feature-flags-testing.module.ts
+++ projects/ngx-feature-flags-testing/src/lib/ngx-feature-flags-testing.module.ts
@@ -1,73 +1,9 @@
 import { NgModule } from '@angular/core';
-import { NgxFeatureFlagsModule, NgxFeatureFlagsService } from 'ngx-feature-flags';
-import { NgxFeatureFlagsConfigurationService } from './ngx-feature-flags-configuration.service';
-
-export function emptyFactory() {
-  return Promise.resolve(new Map<string, boolean>());
-}
+import { NgxFeatureFlagsTestingComponent } from './ngx-feature-flags-testing.component';
 
 @NgModule({
-  imports: [NgxFeatureFlagsModule],
-  providers: [
-    {
-      provide: NgxFeatureFlagsService,
-      // tslint:disable-next-line: typedef
-      useValue: new NgxFeatureFlagsService(emptyFactory),
-    },
-    NgxFeatureFlagsConfigurationService,
-  ],
-  exports: [NgxFeatureFlagsModule],
+  declarations: [NgxFeatureFlagsTestingComponent],
+  imports: [],
+  exports: [NgxFeatureFlagsTestingComponent],
 })
-export class NgxFeatureFlagsTestingModule {
-  private featureFlagConfig: Map<string, boolean> = new Map<string, boolean>();
-  /**
-   * This method offer the ability to initialize the module with multiple feature flags
-   *
-   * @static
-   * @param {Map<string, boolean>} featureFlags defined feature flags
-   * @return {*}  {ModuleWithProviders<NgxFeatureFlagsTestingModule>}
-   * @memberof NgxFeatureFlagsTestingModule
-   */
-  public static withFeatureFlags(featureFlags: Map<string, boolean>): NgxFeatureFlagsTestingModule {
-    const testingModule: NgxFeatureFlagsTestingModule = new NgxFeatureFlagsTestingModule();
-    testingModule.featureFlagConfig = featureFlags;
-
-    return testingModule;
-  }
-
-  /**
-   * This method offer the ability to initialize the module with a single feature flag
-   *
-   * @static
-   * @param {string} featureName the name of the feature flag
-   * @param {boolean} enabled the state of the feature flag
-   * @return {*}  {ModuleWithProviders<NgxFeatureFlagsTestingModule>}
-   * @memberof NgxFeatureFlagsTestingModule
-   */
-  public static withFeatureFlag(featureName: string, enabled: boolean = false): NgxFeatureFlagsTestingModule {
-    const flags: Map<string, boolean> = new Map();
-    flags.set(featureName, enabled);
-
-    return this.withFeatureFlags(flags);
-  }
-  // tslint:disable-next-line: typedef
-  public get providers() {
-    const featureFlagService: NgxFeatureFlagsService = new NgxFeatureFlagsService(this.featureFlagConfigPromise);
-    // tslint:disable-next-line: no-string-literal
-    featureFlagService['featureFlags'] = this.featureFlagConfig;
-
-    return [
-      {
-        provide: NgxFeatureFlagsService,
-        useValue: featureFlagService,
-      },
-      NgxFeatureFlagsConfigurationService,
-    ];
-  }
-  // tslint:disable-next-line: typedef
-  public get ngModule() {
-    return NgxFeatureFlagsTestingModule;
-  }
-  private readonly featureFlagConfigPromise = (): Promise<Map<string, boolean>> =>
-    Promise.resolve(this.featureFlagConfig);
-}
+export class NgxFeatureFlagsTestingModule {}
